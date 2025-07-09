62c7e0194150ae5631c3f0f781cc91490066d085
Mike White <myquite@gmail.com>
2025-07-09 12:39:11 -0500

    Add initial game setup and configuration files

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import pygame
 
 from constants import *
 
-def main():
-    print("Starting Asteroids!")
+def main(): 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()  # Set the frame rate to 60 FPS
+    dt = 0
+    
 
     while True:
         for event in pygame.event.get():
@@ -15,7 +17,9 @@ def main():
             
         screen.fill((0, 0, 0))
         pygame.display.flip()
-    
+
+        dt = clock.tick(60) /1000  # Convert milliseconds to seconds
+
 
 
 if __name__ == "__main__":
