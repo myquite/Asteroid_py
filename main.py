0b69e96ec32225ec5bea8c7e833353bf1be46e6a
Mike White <myquite@gmail.com>
2025-07-09 12:55:15 -0500

    Add player and circle shape classes with drawing functionality

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import pygame
 
 from constants import *
+from player import Player
 
 def main(): 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()  # Set the frame rate to 60 FPS
     dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
 
     while True:
@@ -16,10 +19,12 @@ def main():
                 return
             
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60) /1000  # Convert milliseconds to seconds
 
+       
 
 
 if __name__ == "__main__":
